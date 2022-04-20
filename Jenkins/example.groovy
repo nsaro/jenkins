252ab8623e14b51dc1d6fbef99b9bb28affb2575
Neeraj Kumar Saroya <neeraj.saroya@zuehlke.com>
2022-04-20 16:26:29 +0200

    Update

---
+++ Jenkins/example.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "M3"
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                echo '----------------------Hello World Start From Groovy file ----------------------'
+                // Get some code from a GitHub repository
+                git 'https://github.com/nsaro/jenkins.git'
+
+                // Run Maven on a Unix agent.
+                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+                echo '----------------------Hello World Finish From Groovy file ----------------------'
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                success {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
+
